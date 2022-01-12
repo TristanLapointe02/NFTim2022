30ba69760a8942f63561dbe63e73b98a79a326bd
Andy Ke <andyke00@gmail.com>
2022-01-12 00:16:26 -0500

    ajout buisson, roches, cloture

--- Assets/Scrips/testPointsAndy.cs
+++ Assets/Scrips/testPointsAndy.cs
@@ -10,26 +10,31 @@ public class testPointsAndy : MonoBehaviour
         if (infoCollision.gameObject.tag == "vache")
         {
             scoreManager.instance.pointsVache();
+            Destroy(infoCollision.gameObject);
         }
 
         else if (infoCollision.gameObject.tag == "mouton")
         {
             scoreManager.instance.pointsMouton();
+            Destroy(infoCollision.gameObject);
         }
 
         else if (infoCollision.gameObject.tag == "chien")
         {
             scoreManager.instance.pointsChien();
+            Destroy(infoCollision.gameObject);
         }
 
         else if (infoCollision.gameObject.tag == "cheval")
         {
             scoreManager.instance.pointsCheval();
+            Destroy(infoCollision.gameObject);
         }
 
         else if (infoCollision.gameObject.tag == "lama")
         {
             scoreManager.instance.pointsLama();
+            Destroy(infoCollision.gameObject);
         }
     }
 }
