6bc305c8c2c311878fdd3b78b09350bd67d7dc7d
TristanLapointe02 <tristan.lapointe02@gmail.com>
2022-01-13 13:12:15 -0500

    modifier tir roche (scale/part)

--- Assets/Scrips/GestionVictoireDefaite.cs
+++ Assets/Scrips/GestionVictoireDefaite.cs
@@ -13,18 +13,21 @@ public class GestionVictoireDefaite : MonoBehaviourPunCallbacks
     // Update is called once per frame
     void Update()
     {
-        if (gestionScore.pointage1 > gestionScore.pointage2)
+        if (PhotonNetwork.PlayerList.Length == 2)
         {
-            texteFin.text = PhotonNetwork.PlayerList[0].NickName + " est gagnant!!!";
-        }
-        else if (gestionScore.pointage2 > gestionScore.pointage1)
-        {
-            texteFin.text = PhotonNetwork.PlayerList[1].NickName + " est gagnant!!!";
-        }
-        else
-        {
-            texteFin.text = "Partie nulle";
-        }
+            if (gestionScore.pointage1 > gestionScore.pointage2)
+            {
+                texteFin.text = PhotonNetwork.PlayerList[0].NickName + " est gagnant!!!";
+            }
+            else if (gestionScore.pointage2 > gestionScore.pointage1)
+            {
+                texteFin.text = PhotonNetwork.PlayerList[1].NickName + " est gagnant!!!";
+            }
+            else
+            {
+                texteFin.text = "Partie nulle";
+            }
+        }     
     }
     
     //Quitter la partie
