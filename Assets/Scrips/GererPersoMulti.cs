d624ffc226a90be9034ef0c9c70eea76170dc3a6
Christopher Stevens <91079060+Gcarburant@users.noreply.github.com>
2022-01-12 21:05:56 -0500

    defi 2
    probleme avec la souris

--- Assets/Scrips/GererPersoMulti.cs
+++ Assets/Scrips/GererPersoMulti.cs
@@ -11,14 +11,16 @@ public class GererPersoMulti : MonoBehaviourPunCallbacks
     public Vector3[] positions; //Positions aléatoires où le joueur peut spawn
     public int positionTableau; //Position dans le tableau pigée au hasard quand on fait spawn le joueur
     public static GameObject joueurLocal;
-    public  GameObject quitterPanneau; 
+    public GameObject quitterPanneau;
+    public GameObject[] FR;
+    public GameObject[] EN;
     bool ctrlActive; // est-ce que les commandes sont affichés
     // Start is called before the first frame update
     void Start()
     {
         //Choisir une valeur aléatoire de position de spawn du personnage
         positionTableau = Random.Range(0, 7);
-         //INSTANCIER Edgar
+        //INSTANCIER Edgar
         if (GestionConnexion.PersonnageChoisi == "Edgar")
         {
             joueurLocal = PhotonNetwork.Instantiate("ParentEdgard", positions[positionTableau], Quaternion.identity, 0, null);
@@ -28,29 +30,67 @@ public class GererPersoMulti : MonoBehaviourPunCallbacks
         {
             joueurLocal = PhotonNetwork.Instantiate("ParentJoseph", positions[positionTableau], Quaternion.identity, 0, null);
         }
-
-        //CHANGER NOM JOUEUR LOCAL
+        //CHANGER NOM JOUEUR LOCAL         
         joueurLocal.name = PhotonNetwork.LocalPlayer.NickName;
     }
 
     // Update is called once per frame
     void Update()
     {
-         //Permettre au joueur de quitter avec escape
-        if(Input.GetKeyDown(KeyCode.Escape) && ctrlActive == false){
+        //Permettre au joueur de quitter avec escape
+        if (Input.GetKeyDown(KeyCode.Escape) && ctrlActive == false)
+        {
             ctrlActive = true;
             quitterPanneau.SetActive(true); // ouvrir menu
             Cursor.lockState = CursorLockMode.None;
         }
-        else if(Input.GetKeyDown(KeyCode.Escape) && ctrlActive == true){
+        else if (Input.GetKeyDown(KeyCode.Escape) && ctrlActive == true)
+        {
             ctrlActive = false;
-             Cursor.lockState = CursorLockMode.Locked;
-           quitterPanneau.SetActive(false); // fermer menu
+            Cursor.lockState = CursorLockMode.Locked;
+            quitterPanneau.SetActive(false); // fermer menu
         }
     }
 
-    public void Quitter(){
-       //Fermer l'appli
+    public void Quitter()
+    {
+        //Fermer l'appli
         Application.Quit();
     }
+    public void toutFR()
+    {
+
+
+
+        foreach (GameObject obj in FR)
+        {
+            obj.SetActive(true);
+        }
+
+
+
+        foreach (GameObject obj in EN)
+        {
+            obj.SetActive(false);
+        }
+
+    }
+    public void toutEN()
+    {
+
+
+
+        foreach (GameObject obj in EN)
+        {
+            obj.SetActive(true);
+        }
+
+
+
+        foreach (GameObject obj in FR)
+        {
+            obj.SetActive(false);
+        }
+    }
 }
+
