63e8c07cf7a111c980d732979c5118518f959332
TristanLapointe02 <tristan.lapointe02@gmail.com>
2022-01-13 13:06:13 -0500

    fix variable introuvable fin

--- Assets/Scrips/ProprietaireAnimal.cs
+++ Assets/Scrips/ProprietaireAnimal.cs
@@ -29,7 +29,7 @@ public class ProprietaireAnimal : MonoBehaviourPunCallbacks
          if((joueurAvecAnimal.gameObject.tag != "mouton" || joueurAvecAnimal.gameObject.tag != "cochon"
         || joueurAvecAnimal.gameObject.tag != "cheval" || joueurAvecAnimal.gameObject.tag != "chien"
         || joueurAvecAnimal.gameObject.tag != "lama" || joueurAvecAnimal.gameObject.tag != "vache"
-        || joueurAvecAnimal.gameObject.tag != "zebre") && photonView.IsMine && tiensAnimal == true && joueurAvecAnimal != null)
+        || joueurAvecAnimal.gameObject.tag != "zebre") && photonView.IsMine && tiensAnimal == true && joueurAvecAnimal != null && TimerPartieMultiplayer.partieCommencer == true)
         {
             gameObject.transform.position =  joueurAvecAnimal.transform.position;
             // La ligne commenté explose la vitesse des autres animaux, faudrait trouver un moyen de target seulement l'animal picked up
