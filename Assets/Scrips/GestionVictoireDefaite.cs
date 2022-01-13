fea4a043b092ccf2b673a7987290243eff77eccc
William Foucher <e1938665@cmaisonneuve.qc.ca>
2022-01-13 10:48:34 -0500

    scene Fin

--- Assets/Scrips/GestionVictoireDefaite.cs
+++ Assets/Scrips/GestionVictoireDefaite.cs
@@ -1,19 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Realtime;
+using Photon.Pun;
 
-public class GestionVictoireDefaite : MonoBehaviour
+public class GestionVictoireDefaite : MonoBehaviourPunCallbacks
 {
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    public GameObject texteVictoire;
+    public GameObject texteDefaite;
+   
     // Update is called once per frame
     void Update()
     {
-    
+    if (PhotonNetwork.LocalPlayer == PhotonNetwork.PlayerList[0] && photonView.IsMine)
+            {
+                print("Je suis joueur 0");
+                if(gestionScore.pointage1 > gestionScore.pointage2){
+                    texteVictoire.SetActive(true);
+                    texteDefaite.SetActive(false);
+                    print(" joueur 0 à gagné");
+                }
+                else if(gestionScore.pointage1 < gestionScore.pointage2){
+                    texteVictoire.SetActive(false);
+                    texteDefaite.SetActive(true);
+                }else{
+                    texteVictoire.SetActive(true);
+                    texteDefaite.SetActive(false);
+                }
+            }
+    if (PhotonNetwork.LocalPlayer == PhotonNetwork.PlayerList[1] && photonView.IsMine)
+        {
+            print("Je suis joueur 1");
+            if(gestionScore.pointage2 > gestionScore.pointage1){
+                    texteVictoire.SetActive(true);
+                    texteDefaite.SetActive(false);
+                    print(" joueur 1 à gagné");
+                }
+                else if(gestionScore.pointage2 < gestionScore.pointage1){
+                    texteVictoire.SetActive(false);
+                    texteDefaite.SetActive(true);
+                }else{
+                    texteVictoire.SetActive(true);
+                    texteDefaite.SetActive(false);
+                }
+        }
+    }
+    //Quitter la partie
+    public void QuitterPartie()
+    {
+        //Fermer l'appli
+        Application.Quit();
     }
 }
