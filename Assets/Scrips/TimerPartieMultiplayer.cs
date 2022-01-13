e9ac877e4036baff104dd5b81da52d7c0b48d96a
TristanLapointe02 <tristan.lapointe02@gmail.com>
2022-01-13 13:03:32 -0500

    fix potentiel scene fin

--- Assets/Scrips/TimerPartieMultiplayer.cs
+++ Assets/Scrips/TimerPartieMultiplayer.cs
@@ -20,7 +20,6 @@ public class TimerPartieMultiplayer : MonoBehaviourPunCallbacks
             timer -= Time.deltaTime;
             timerAvantFin.text = Mathf.RoundToInt(timer).ToString(); //modifie le texte en string
             if(timer <= 0f){
-                PhotonNetwork.LeaveRoom();
                 PhotonNetwork.LoadLevel("Fin");
                 SceneManager.LoadScene("Fin");
                 partieCommencer = false;
